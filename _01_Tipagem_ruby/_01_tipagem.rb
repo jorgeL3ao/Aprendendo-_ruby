69966ee3ddfb12528a516ccedc7cd2e520e0fcc6
jorgeL3ao <jorgesalgado1190@gmail.com>
2023-03-02 10:47:37 -0300

    Evoluindo no aprendizado dos arrays

--- _01_Tipagem_ruby/_01_tipagem.rb
+++ _01_Tipagem_ruby/_01_tipagem.rb
@@ -1,16 +1,20 @@
 =begin
+ Oque é tipo de dados?
+ - Classificação dos dados que iremos utilizar em nosso progama
+
  Tipagem dinâmica é uma característica de determinadas linguagens de programação, que não exigem declarações de tipos de dados, pois são capazes de escolher que tipo utilizar dinamicamente para cada variável,
 podendo alterá-lo durante a compilação ou a execução do programa.
 =end
 
 # Code
+puts "Exemplo 1"
 v = "teste"
 puts v.class
 
 v = 1
 puts v.class
 
-# Tipagem Forte ou seja exige que o tipo de dado de um valor seja do mesmo tipo da variável ao qual este valor será atribuído.
+#  Tipagem Forte ou seja exige que o tipo de dado de um valor seja do mesmo tipo da variável ao qual este valor será atribuído.
 
 i = 1
 s = 'oi'
--- _02_Array_ruby/_02_funcoes_do_array.rb
+++ _02_Array_ruby/_02_funcoes_do_array.rb
@@ -1,7 +1,6 @@
 =begin
   Para adicionar elementos em um Array, podemos utilizar o método push ou o << (lembram desse, nas Strings?), desse
 modo:
-
 =end
 a = %w(john paul george ringo)
 puts "-=-=-=" 
@@ -34,3 +33,21 @@ puts "-=-=-=-=-"
 ex1 = ["a", "b", "c"] | ["c", "d", "a"]
 puts ex1 
 
+# Para saber os elementos que são diferentes entre um Array e outro, também na versão 2.6 foi introduzido o método
+# "difference" -> que também era utilizado o operador - para a mesma finalidade:
+
+ex2 = ["a", "a", "b", "b", "c", "c", "d", "e"].difference ["a", "b", "d"]
+puts ex2
+
+# E para completar, saber os elementos que são comuns entre um Array e outro, na versão 2.7 foi introduzido o método
+# "intersection", que também era utilizado o operador bitwise AND & para a mesma finalidade
+
+ex3 =  ["a", "b", "c", "d"].intersection ["b", "d"]
+
+
+# Pesquisando em um Array com metodo Dig 
+array = [0, [1, [2, 3]]]
+puts array
+puts "-=-=-=-=-"
+array.dig(1,1,0) 
+puts array 
